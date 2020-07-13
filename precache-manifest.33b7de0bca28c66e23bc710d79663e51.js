ee0a8ce4ae87ff64f01caa9221a896bbdfae4149
Deep Muni <deep.muni94@gmail.com>
2020-07-13 20:33:16 -0300

    Updates

---
+++ precache-manifest.33b7de0bca28c66e23bc710d79663e51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5789b767c0825c60546f195ad0ea7bf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c0584a59d580e8f0b35",
+    "url": "/static/css/main.1a97d6b5.chunk.css"
+  },
+  {
+    "revision": "792fee5172162eebd94a",
+    "url": "/static/js/2.1f6a0a2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f6a0a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0584a59d580e8f0b35",
+    "url": "/static/js/main.918cf4c6.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
